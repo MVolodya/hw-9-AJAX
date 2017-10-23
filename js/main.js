9a097a638b0f197457aa6d125f9a0016bc525b4f
MVolodya <medvid.volodya@gmail.com>
2017-10-24 02:51:22 +0300

    updated design & added animatin for loader

--- js/main.js
+++ js/main.js
@@ -19,20 +19,28 @@ const cors_api_host = "https://cors-anywhere.herokuapp.com/";
 
 
 
-let button = () => {
-    let $beforeDay = document.querySelector('#beforeDay');
-    $beforeDay.addEventListener('click', function() {
-        currentData = moment.unix(currentData).subtract(1, 'days').format('X');
-        loadCity(latitude, longitude, currentData);
-    });
-
+let nextDay = () => {
     let $nextDay = document.querySelector('#nextDay');
+    let $btnPrimary = document.querySelector('.btn-primary');
+
+    if (moment.unix(currentData).format("LL") == moment().format("LL")) {
+        $nextDay.setAttribute("disabled", "");
+        $btnPrimary.style.cursor = "default";
+    }
+
     $nextDay.addEventListener('click', function() {
         currentData = moment.unix(currentData).add(1, 'days').format('X');
         loadCity(latitude, longitude, currentData);
     });
 }
 
+let beforeDay = () => {
+    let $beforeDay = document.querySelector('#beforeDay');
+    $beforeDay.addEventListener('click', function() {
+        currentData = moment.unix(currentData).subtract(1, 'days').format('X');
+        loadCity(latitude, longitude, currentData);
+    });
+}
 
 
 let renderDOM = (summary, temperature, windSpeed, humidity, icon, cityName) => {
@@ -43,27 +51,25 @@ let renderDOM = (summary, temperature, windSpeed, humidity, icon, cityName) => {
     icon = icon.toUpperCase().replace(/-/g, "_");;
 
     let textData =
-    `<div class="card" style="width: 20rem;">
-        <div class="card-body box">
-            <h4 class="card-title">${cityName}</h4>
-            <h6 class="card-subtitle mb-2 text-muted">${moment.unix(currentData).format("LL")}</h6>
-            <div class="card-text">
-                <div class="details">
-                    <h2 id="temperature">${parseInt(temperature) + '\u2103'}</h2>
-                    <div class="main-info">
-                        <canvas id="icon1" width="75" height="75"></canvas>
-                        <div id="summary">${summary}</div>
-                    </div>
-                </div>
-                <div class="other-info">
-                    <div id="windSpeed">Вітер: ${windSpeed}м/c</div>
-                    <div id="humidity">Вологість: ${humidity*100}%</div>
+    `<div class="card-body ">
+        <h4 class="card-title">${cityName}</h4>
+        <h6 class="card-subtitle mb-2 text-muted">${moment.unix(currentData).format("LL")}</h6>
+        <div class="card-text">
+            <div class="details">
+                <h2 id="temperature">${parseInt(temperature) + '\u2103'}</h2>
+                <div class="main-info">
+                    <canvas id="icon1" width="75" height="75"></canvas>
+                    <div id="summary">${summary}</div>
                 </div>
             </div>
-            <div class="buttons">
-                <button id="beforeDay" type="button" class="btn btn-outline-secondary">Назад</button>
-                <button id="nextDay" type="button" class="btn btn-outline-primary">Вперед</button>
+            <div class="other-info">
+                <div id="windSpeed">Вітер: ${windSpeed}м/c</div>
+                <div id="humidity">Вологість: ${humidity*100}%</div>
             </div>
+        </div>
+        <div class="buttons">
+            <button id="beforeDay" type="button" class="btn btn-secondary">Назад</button>
+            <button id="nextDay" type="button" class="btn btn-primary">Вперед</button>
         </div>
     </div>`;
 
@@ -72,7 +78,22 @@ let renderDOM = (summary, temperature, windSpeed, humidity, icon, cityName) => {
     skycons.add("icon1", Skycons[icon]);
     skycons.play();
 
-    button();
+    beforeDay();
+    nextDay();
+}
+
+let startLoaderAnim = () => {
+    let $loader = document.querySelector('.spinner-block');
+    let $box    = document.querySelector('.box');
+    $loader.classList.remove("visible");
+    $box.style.opacity = 0.3;
+}
+
+let endLoaderAnim = () => {
+    let $loader = document.querySelector('.spinner-block');
+    let $box    = document.querySelector('.box');
+    $loader.classList.add("visible");
+    $box.style.opacity = 1;
 }
 
 
@@ -92,7 +113,9 @@ let loadData = (latitude, longitude, currentData, cityName) => {
 
             console.log(data);
 
-            renderDOM(summary, temperature, windSpeed, humidity, icon, cityName)
+            renderDOM(summary, temperature, windSpeed, humidity, icon, cityName);
+
+            endLoaderAnim();
         })
         .catch(e => console.log("Oops, error ::: " + e))
 
@@ -105,6 +128,9 @@ let loadCity = (latitude, longitude, currentData) => {
     fetch(url)
         .then(response => response.json())
         .then(city => {
+
+            startLoaderAnim();
+
             const cityName = city.results[1].address_components[2].short_name;
 
             loadData(latitude, longitude, currentData, cityName)
@@ -116,7 +142,7 @@ let loadCity = (latitude, longitude, currentData) => {
 
     const options = {
         enableHighAccuracy: true,
-        timeout: 5000,
+        timeout: 100000,
         maximumAge: 0
     };
 
